5531d145c7cd108888f2d308fc1d586b19dddc5b
David Cavazos <dcavazos@google.com>
2020-08-06 10:13:19 -0700

    Optimized tests for typecheckP

--- tests/Validate/TypecheckPTest.elm
+++ tests/Validate/TypecheckPTest.elm
@@ -1,5 +1,6 @@
 module Validate.TypecheckPTest exposing (suite)
 
+import Dict exposing (Dict)
 import Expect
 import FVM exposing (Error(..), Expression(..), Pattern(..), Type(..), new)
 import FVM.Validate exposing (typecheckP)
@@ -9,21 +10,21 @@ import Test exposing (Test, describe, test)
 suite : Test
 suite =
     describe "typecheckP"
-        [ test "(_ : X) : Int -- TypeNotFound" <|
+        [ test "(_ : X) : Int -- TypeNotFound -- check pattern" <|
             \_ ->
                 FVM.new
                     |> typecheckP (AnyP (NameT "X" [])) IntT
                     |> Expect.equal (Err (TypeNotFound "X"))
 
         --
-        , test "(_ : Int) : X -- TypeNotFound" <|
+        , test "(_ : Int) : X -- TypeNotFound -- check type" <|
             \_ ->
                 FVM.new
                     |> typecheckP (AnyP IntT) (NameT "X" [])
                     |> Expect.equal (Err (TypeNotFound "X"))
 
         --
-        , test "(_ : Int) : Number -- PatternMismatch" <|
+        , test "(_ : Int) : Number -- PatternMismatch -- pattern not equal" <|
             \_ ->
                 FVM.new
                     |> typecheckP (AnyP IntT) NumberT
@@ -36,17 +37,31 @@ suite =
                     |> typecheckP (AnyP IntT) IntT
                     |> Expect.equal (Ok (AnyP IntT))
 
-        --
-        , test "(_ : Int) : Type | Number -- PatternMismatch" <|
+        -- RecordP
+        , test "{} : {} -- ok" <|
             \_ ->
                 FVM.new
-                    |> typecheckP (AnyP IntT) (UnionT [ TypeT, NumberT ])
-                    |> Expect.equal (Err (PatternMismatch (AnyP IntT) (UnionT [ TypeT, NumberT ])))
+                    |> typecheckP (RecordP Dict.empty) (RecordT Dict.empty)
+                    |> Expect.equal (Ok (RecordP Dict.empty))
 
-        -- --
-        -- , test "(_ : Int) : Int | Number -- ok" <|
-        --     \_ ->
-        --         FVM.new
-        --             |> typecheckP (AnyP IntT) (UnionT [ IntT, NumberT ])
-        --             |> Expect.equal (Ok (AnyP IntT))
+        --
+        , test "{x : Int} : {} -- PatternMismatch -- missing field" <|
+            \_ ->
+                FVM.new
+                    |> typecheckP (RecordP (Dict.singleton "x" IntT)) (RecordT Dict.empty)
+                    |> Expect.equal (Err (PatternMismatch (RecordP (Dict.singleton "x" IntT)) (RecordT Dict.empty)))
+
+        --
+        , test "{x : Int} : {x : Number} -- PatternMismatch -- field with type mismatch" <|
+            \_ ->
+                FVM.new
+                    |> typecheckP (RecordP (Dict.singleton "x" IntT)) (RecordT (Dict.singleton "x" NumberT))
+                    |> Expect.equal (Err (PatternMismatch (RecordP (Dict.singleton "x" IntT)) (RecordT (Dict.singleton "x" NumberT))))
+
+        --
+        , test "{} : {x : Int} -- ok" <|
+            \_ ->
+                FVM.new
+                    |> typecheckP (RecordP Dict.empty) (RecordT (Dict.singleton "x" IntT))
+                    |> Expect.equal (Ok (RecordP Dict.empty))
         ]
